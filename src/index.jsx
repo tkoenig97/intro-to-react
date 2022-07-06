8c14851d70d1369272306e884099c47706c8b1ac
tkoenig97 <tylerskoenig@gmail.com>
2022-07-06 14:00:20 -0500

    Added turn functionality

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-function Squaree(props) {
+function Square(props) {
     return (
         <button className='square' onClick={props.onClick}>
             {props.value}
@@ -15,13 +15,17 @@ function Squaree(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X'
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
     }
 
     renderSquare(i) {
